184ce4c0d33096baf6b8c1ef8de0fb1e748de9dc
Stefano Mandelli <linuxfree2@gmail.com>
2016-03-02 15:40:39 +0100

    On branch master  Your branch is ahead of 'origin/master' by 3 commits.    (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   main.R

--- example_class/main.R
+++ example_class/main.R
@@ -2,4 +2,5 @@ library(methods)
 source("ClassePunto.R")
 
 punti <- new("punto", x = -4:4, y = exp(-4:4) )
-plot(slot(punti,"x"),slot(punti,"y"))
+#plot(slot(punti,"x"),slot(punti,"y"))
+plot(punti@x,punti@y)
